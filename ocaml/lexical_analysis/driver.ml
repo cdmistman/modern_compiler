2b8bdee5730769d6b48c78c19eb9b14c2f34a460
Colton Donnelly <colton@donn.io>
2022-11-29 12:20:00 -0500

    use print_endline instead of print_string with a newline concat

--- ocaml/lexical_analysis/driver.ml
+++ ocaml/lexical_analysis/driver.ml
@@ -2,7 +2,7 @@ module Parse = struct
   let parse filename =
     let rec print_tokens lexer =
       let token = Tiger.read lexer in
-      print_string (token ^ "\n");
+      print_endline token;
       if String.sub token 0 3 = "EOF" then () else print_tokens lexer
     in
     open_in filename |> Lexing.from_channel |> print_tokens
