6f350a4bcfd6eb54df706d4456293e7b85d8d8bf
Colton Donnelly <colton@donn.io>
2022-11-29 18:28:11 -0500

    it's actually simplest to do all textbook code in the same directory :)

--- ocaml/tiger/driver.ml
+++ ocaml/tiger/driver.ml
@@ -1,7 +1,7 @@
 module Parse = struct
   let parse filename =
     let rec print_tokens lexer =
-      let token = Tiger.read lexer in
+      let token = Lex.read lexer in
       print_endline token;
       if String.sub token 0 3 = "EOF" then () else print_tokens lexer
     in
