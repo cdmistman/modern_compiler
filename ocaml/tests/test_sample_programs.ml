0df652a4ee76b39c83fe3758abc435d9775af0bf
Colton Donnelly <colton@donn.io>
2022-12-20 14:41:33 -0800

    extend test suite to make testing easier and support expect-fails

--- ocaml/tests/test_sample_programs.ml
+++ ocaml/tests/test_sample_programs.ml
@@ -1,14 +1,47 @@
+open Base
 open OUnit2
 open Tiger
 
 let sample_programs = Programs.sample_programs
 
-let test_parsing =
-  (* parse_test just ensures there's no exception thrown, matching `parsetest.sml` *)
-  let parse_test input _ = ignore @@ Parser.parse_string input in
-  let gen_parse_test (test_name, test_input) =
-    test_name >:: parse_test test_input
-  in
-  "parsing" >::: List.map gen_parse_test sample_programs
+type program_test = string * string
 
-let () = run_test_tt_main ("suite" >::: [ test_parsing ])
+and 'a stage_tests = {
+  runner : string -> 'a;
+  fail : program_test list;
+  pass : program_test list;
+}
+
+let test_stage ?after ~failing runner =
+  let to_test =
+    Option.value_map after ~default:sample_programs ~f:(fun f -> f.pass)
+  in
+  let split_assoc mems =
+    List.fold ~init:([], []) ~f:(fun (l, r) ((test_name, _) as t) ->
+        if List.mem mems test_name ~equal:String.equal then (t :: l, r)
+        else (l, t :: r))
+  in
+  let fail, pass = split_assoc failing to_test in
+  { runner; fail; pass }
+
+let parsing = test_stage ~failing:[ "test49" ] Parser.parse_string
+
+let mk_suite name stage =
+  let failing = stage.fail in
+  let passing = stage.pass in
+  let runner = stage.runner in
+  let failing_runner test =
+    fst test >:: fun _ ->
+    assert_bool "Expected test to fail"
+    @@ Exn.does_raise (fun () -> ignore @@ runner @@ snd test)
+  in
+  let passing_runner test =
+    fst test >:: fun _ -> ignore @@ runner @@ snd test
+  in
+  [
+    name ^ " (pass)" >::: List.map ~f:passing_runner passing;
+    name ^ " (fail)" >::: List.map ~f:failing_runner failing;
+  ]
+
+let () =
+  run_test_tt_main ("suite" >::: List.concat [ mk_suite "parsing" parsing ])
